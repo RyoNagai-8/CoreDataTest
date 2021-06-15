083af7d1063d1a27926bde59cb70ae1d99c65479
RyoNagai <59611366+RyoNagai-8@users.noreply.github.com>
2021-06-15 16:46:52 +0900

    Edit storyboard Add CoreData

--- CoreDataTest/ViewController.swift
+++ CoreDataTest/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var age: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func create(_ sender: Any) {
+    }
+    
+    @IBAction func read(_ sender: Any) {
+    }
+    @IBAction func update(_ sender: Any) {
+    }
+    @IBAction func deleteButton(_ sender: Any) {
+    }
+    
 }
 
